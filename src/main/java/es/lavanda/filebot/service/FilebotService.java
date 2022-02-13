c59049db21ad5912797556cddb4e3071c3f37ee2
Luis Carlos <luisca_jl@hotmail.com>
2022-02-13 12:00:55 +0100

    Updated

--- src/main/java/es/lavanda/filebot/service/FilebotService.java
+++ src/main/java/es/lavanda/filebot/service/FilebotService.java
@@ -1,24 +1,11 @@
 package es.lavanda.filebot.service;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import es.lavanda.filebot.exception.FilebotException;
 import es.lavanda.filebot.model.Filebot;
-import es.lavanda.filebot.model.FilebotFile;
-import es.lavanda.filebot.repository.FilebotFileRepository;
 import es.lavanda.filebot.repository.FilebotRepository;
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,6 +17,7 @@ public class FilebotService {
     private FilebotRepository filebotRepository;
 
     public Page<Filebot> getAllPageable(Pageable pageable) {
+        log.info("getAllPageable");
         return filebotRepository.findAllByOrderByLastModifiedAtDesc(pageable);
     }
 
