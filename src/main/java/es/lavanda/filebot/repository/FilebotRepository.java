ce9a2fd8eb1c54a0610b5e5a437c051a161bd562
Luis Carlos <luisca_jl@hotmail.com>
2023-02-09 22:39:56 +0100

    Filebot CSV export

--- src/main/java/es/lavanda/filebot/repository/FilebotRepository.java
+++ src/main/java/es/lavanda/filebot/repository/FilebotRepository.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Repository;
 
 import es.lavanda.filebot.model.Filebot;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
 
 @Repository
 public interface FilebotRepository extends PagingAndSortingRepository<Filebot, String> {
 
     Page<Filebot> findAllByOrderByLastModifiedAtDesc(Pageable pageable);
-    
+
+    List<Filebot> findAll();
 }
